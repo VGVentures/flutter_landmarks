6b816f60060ac7525917f4996dfd7cd752df8471
Kevin Gray <Kevin@GoPosse.com>
2019-06-04 14:23:12 -0400

    added landmarks data

--- lib/models/data.dart
+++ lib/models/data.dart
@@ -3,8 +3,15 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:landmarks_flutter/models/landmark.dart';
 
-final List<Landmark> landmarkData = load('landmarkData.json');
+final List<Landmark> landmarkData = load('landmarkData.json', (rawData) {
+  return (rawData as List)
+      .map<Landmark>((map) => Landmark.fromJSON(map))
+      .toList();
+});
 
-T load<T>(String filename) {
-  print(rootBundle.loadString(filename));
+T load<T>(String filename, T Function(dynamic) builder) {
+  rootBundle.loadString('assets/$filename').then((fileString) {
+    return builder(json.decode(fileString));
+  });
+  return null;
 }
--- lib/models/landmark.dart
+++ lib/models/landmark.dart
@@ -1,36 +1,58 @@
-//class Landmark: Hashable, Codable, Identifiable {
-//var id: Int
-//var name: String
-//fileprivate var imageName: String
-//fileprivate var coordinates: Coordinates
-//var state: String
-//var park: String
-//var category: Category
-//var isFavorite: Bool
-//
-//var locationCoordinate: CLLocationCoordinate2D {
-//CLLocationCoordinate2D(
-//latitude: coordinates.latitude,
-//longitude: coordinates.longitude)
-//}
-//
-//func image(forSize size: Int) -> Image {
-//ImageStore.shared.image(name: imageName, size: size)
-//}
-//
-//enum Category: String, CaseIterable, Codable, Hashable {
-//case featured = "Featured"
-//case lakes = "Lakes"
-//case rivers = "Rivers"
-//case mountains = "Mountains"
-//}
-//}
-//
-//struct Coordinates: Hashable, Codable {
-//var latitude: Double
-//var longitude: Double
-//}
+enum Category {
+  featured,
+  lakes,
+  rivers,
+  mountains,
+}
 
 class Landmark {
+  final int id;
+  final String name;
+  final String imageName;
+  final Coordinates coordinates;
+  final String state;
+  final String park;
+  final Category category;
+  bool isFavorite;
 
+  Landmark({
+    this.id,
+    this.name,
+    this.imageName,
+    this.coordinates,
+    this.state,
+    this.park,
+    this.category,
+    this.isFavorite,
+  });
+
+  factory Landmark.fromJSON(Map map) {
+    return Landmark(
+        id: map['id'],
+        name: map['name'],
+        imageName: map['imageName'],
+        coordinates: Coordinates.fromJSON(map['coordinates']),
+        state: map['state'],
+        park: map['park'],
+        category: Category.values.firstWhere((e) =>
+            '${map['category']}'.toLowerCase() == (e.toString().split('.')[1])),
+        isFavorite: map['isFavorite']);
+  }
+}
+
+class Coordinates {
+  final double latitude;
+  final double longitude;
+
+  Coordinates({
+    this.latitude,
+    this.longitude,
+  });
+
+  factory Coordinates.fromJSON(Map map) {
+    return Coordinates(
+      latitude: map['latitude'],
+      longitude: map['longitude'],
+    );
+  }
 }
