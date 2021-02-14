f21097243ac2cdfafb2b4f99cf951cfd29e24b34
Kirk Wight <kwight@kwight.ca>
2021-02-14 13:07:26 -0800

    Main: Initialize WidgetsBinding before running the app.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:landmarks_flutter/views/landmark_cell.dart';
 import 'package:landmarks_flutter/models/data.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   loadData().then((_) {
     runApp(MyApp());
   });
